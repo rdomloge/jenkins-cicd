f73fbfe8edd010b4d0ce09defcb4c7bfb19d9db1
Ramsay Domloge <ramsay.domloge@bca.com>
2019-03-30 09:24:06 +0000

    Fixed running Docker from Jenkins

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -p 8282:8080 ramsay:blue-ocean-java
+#docker run -p 8282:8080 ramsay:blue-ocean-java
+docker run -v /var/run/docker.sock:/var/run/docker.sock --group-add 978 -p 8282:8080 --name jenkins_blueocean ramsay:blue-ocean-java
